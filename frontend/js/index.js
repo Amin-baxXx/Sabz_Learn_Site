6c097d7aad78d553f29c27ab4e5c59c273c12312
Amin-baxXx <aminbaxxxx@gmail.com>
2025-01-18 20:02:25 +0330

    Fix bug

---
+++ frontend/js/index.js
@@ -0,0 +1,38 @@
+"use strict";
+const $ = document;
+const landing__title = $.querySelector(".landing__title");
+const landingstatus__count = $.querySelectorAll(".landing-status__count");
+// Load function
+window.addEventListener("load", () => {
+  // typewriter Loader
+  const landingtextContent = "ما به هر قیمتی دوره آموزشی تولید نمیکنیم!";
+  let index = 0;
+  typewriter(landingtextContent, index);
+  // numWriter Loader
+  landingstatus__count.forEach((item) => {
+    numWriter(item);
+  });
+});
+
+const typewriter = function (text, index) {
+  if (text.length > index) {
+    landing__title.textContent += text[index];
+    index++;
+  } else return;
+
+  setTimeout(() => {
+    typewriter(text, index);
+  }, 100);
+};
+
+// Implement animated number counter
+const numWriter = function (item) {
+  const updateNumber = (num = 0) => {
+    item.textContent = num.toLocaleString("fa-IR");
+    num++;
+    if (+item.dataset.count >= +num) {
+      requestAnimationFrame(() => updateNumber(num));
+    }
+  };
+  updateNumber();
+};
---
+++ frontend/js/swiper.js~
@@ -0,0 +1,23 @@
+"use strict";
+const swiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  speed: 800,
+  loop: true,
+  breakpoints: {
+    576: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 3,
+    },
+    grabCursor: true,
+  },
+  // autoplay: {
+  //   delay: 4000,
+  //   disableOnInteraction: false,
+  //   pauseOnMouseEnter: true, // توقف حرکت هنگام هاور
+  // },
+});
